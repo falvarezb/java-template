98dbe30d5c3e902241432f556b50666ac5467753
Francisco Alvarez <41089629+falvarezb@users.noreply.github.com>
2021-10-17 10:09:14 +0100

    initial commit

---
+++ src/main/java/falvarezb/App.java
@@ -0,0 +1,8 @@
+package falvarezb;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    @DisplayName("my test example")
+    public void mytest() {
+        assertEquals(1,1);
+    }
+}
